db2e9fca60bff3ecfd0081af7bf6b9f3da9f0926
aymeneliyas <88336016+aymeneliyas@users.noreply.github.com>
2023-03-24 17:46:12 +0300

    added kth-largest-element-in-an-array.python

--- kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array.py
@@ -1,27 +1,25 @@
 class Solution:
-    def merge_sort(self,left, right,arr):
+    def partition(self,l,r,nums):
+        pivot = nums[r]
+        i = l-1
         
-        if left == right:
-            return [arr[left]]
-        mid = left + (right-left) // 2
-        left_half = self.merge_sort(left,mid,arr)
-        right_half = self.merge_sort(mid+1,right,arr)
+        for j in range(l,r):
+            if nums[j] <= pivot:
+                i += 1
+                nums[i],nums[j] = nums[j],nums[i]
+        nums[i + 1], nums[r] = nums[r], nums[i + 1]
+        
+        return i + 1
+    
+    def quick_sort(self,l,r,nums):
+        if l >= r:
+            return
 
-        arr = []
-        l = 0
-        r = 0
-        while l < len(left_half) and r < len(right_half):
-            if left_half[l] > right_half[r]:
-                arr.append(left_half[l])
-                l += 1
-            else:
-                arr.append(right_half[r])
-                r += 1
-        
-        arr.extend(right_half[r:])
-        arr.extend(left_half[l:])
-        return arr      
+        mid = self.partition(l,r,nums)
+        left = self.quick_sort(l,mid-1,nums)
+        right = self.quick_sort(mid+1,r,nums)
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        arr = self.merge_sort(0,len(nums)-1,nums)
-        return arr[k-1]
+        self.quick_sort(0,len(nums)-1,nums)
+        print(nums)
+        return nums[-k]
